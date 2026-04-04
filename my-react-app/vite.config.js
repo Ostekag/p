02c29d473464db7a8860574c301e962bc4ec89d0
ostekage <rominaiska@gmail.com>
2026-04-04 13:19:40 +0500

    fix: set base path for gh-pages

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/p/', // Название вашего репозитория
+  base: '/p/', // <-- ОБЯЗАТЕЛЬНО ДОБАВЬ ЭТУ СТРОЧКУ
 })
